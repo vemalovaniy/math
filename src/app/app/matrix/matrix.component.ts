7960f924ec1da5d41d4e24c2846cbc6b89bc7906
vadim.malovaniy <vemalovaniy@gmail.com>
2024-06-29 20:58:37 +0300

    added on example

--- src/app/app/matrix/matrix.component.ts
+++ src/app/app/matrix/matrix.component.ts
@@ -4,7 +4,15 @@ import { MatrixUiComponent } from 'src/app/components';
 import { Matrix } from 'src/core';
 import { expectedMultiplyedMatrix, expectedMultiplyedMatrix2, multiplyMatrix, multiplyMatrix2, multiplyMatrix21, multiplyValue } from './data-set';
 
+const testMatrix2x2 = [
+  [1, 2],
+  [3, 4],
+]
 
+const inverseExpected2x2 = [
+  [-2, 1],
+  [1.5, -0.5],
+]
 @Component({
   selector: 'math-matrix',
   standalone: true,
@@ -59,6 +67,12 @@ export class MatrixComponent {
     init2: multiplyMatrix21,
     expected: expectedMultiplyedMatrix2,
     result: new Matrix(multiplyMatrix21).multiply(multiplyMatrix2).value,
+  }
+
+  public inverseByMatrixData = {
+    init: testMatrix2x2,
+    expected: inverseExpected2x2,
+    result: new Matrix(testMatrix2x2).inverse().value,
   }
 
   public matrix2Result = new Matrix(this.matrix21).addition(this.matrix2).value;
